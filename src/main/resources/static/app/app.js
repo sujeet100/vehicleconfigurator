6b03b33c5b533aa60803d7123eba292f60634e46
Kamthe <kamthes@pun-kamthes-m.ds.ad.adp.com>
2016-07-31 17:20:43 +0530

    Initial import

---
+++ src/main/resources/static/app/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularappApp
+ * @description
+ * # angularappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('configuratorApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/home/view/home.html',
+        controller: 'HomeCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ src/main/resources/static/app/home/controller/HomeCtrl.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularappApp
+ */
+angular.module('configuratorApp')
+  .controller('HomeCtrl', function ($scope) {
+    $scope.message = "Our vehicle configurator";
+  });
